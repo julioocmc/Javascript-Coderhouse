d9d7bfddd5d0bdb9d529074ba7a870ada2711c9f
Julio Mijares <112848668+julioocmc@users.noreply.github.com>
2023-03-15 15:42:27 -0400

    Update index.js

--- PreEntrega3Mijares/index.js
+++ PreEntrega3Mijares/index.js
@@ -1,11 +1,76 @@
 //-----------Tercera Entrega--------
-let input = document.getElementsByClassName('form_item');
-for (let i = 0; i < input.length; i++) {
-  input[i].addEventListener('keyup', function () {
-    if (this.value.length >= 1) {
-      this.nextElementSibling.classList.add('pin');
-    } else {
-      this.nextElementSibling.classList.remove('pin');
-    }
-  });
-}
+
+const listaClientes = document.querySelector('#lista-clientes');
+const formulario = document.querySelector('#formulario');
+
+let clientes = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (JSON.parse(localStorage.getItem('clientes')) == 'null') {
+    clientes = [];
+  } else {
+    clientes = JSON.parse(localStorage.getItem('clientes'));
+  }
+  render();
+  document.querySelectorAll('#lista-clientes p a');
+});
+
+const resetHTML = () => {
+  while (listaClientes.firstChild) {
+    listaClientes.removeChild(listaClientes.firstChild);
+  }
+};
+
+const sincronizarStorage = () => {
+  localStorage.setItem('clientes', JSON.stringify(clientes));
+};
+
+const render = () => {
+  resetHTML();
+
+  if (clientes.length > 0) {
+    clientes.forEach((cliente) => {
+      const btnDelete = document.createElement('a');
+      btnDelete.classList = 'borrar-nombre';
+      btnDelete.textContent = '❌';
+      const p = document.createElement('p');
+      p.textContent = cliente.nombre;
+      p.id = cliente.id;
+      p.appendChild(btnDelete);
+      listaClientes.appendChild(p);
+    });
+  }
+
+  sincronizarStorage();
+};
+
+const agregarCliente = (evt) => {
+  evt.preventDefault();
+
+  const cliente = document.querySelector('#cliente').value;
+  console.log(cliente);
+  if (cliente.length <= 2) {
+    alert('Debe escribir el nombre completo, intente de nuevo ');
+    return;
+  }
+
+  const clienteObj = {
+    id: Date.now(),
+    nombre: cliente,
+  };
+  clientes.push(clienteObj);
+
+  render();
+
+  formulario.reset();
+};
+
+const borrarCliente = (evt) => {
+  evt.preventDefault();
+  let id = evt.target.parentElement.id;
+  clientes = clientes.filter((cliente) => cliente.id != id);
+  render();
+};
+
+formulario.addEventListener('submit', agregarCliente);
+listaClientes.addEventListener('click', borrarCliente);
