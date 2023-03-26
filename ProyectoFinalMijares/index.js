9ca3f965239e41c7839bef3ddbdb265d15ab8ac4
Julio Mijares <112848668+julioocmc@users.noreply.github.com>
2023-03-26 00:19:38 -0400

    Responsive

--- ProyectoFinalMijares/index.js
+++ ProyectoFinalMijares/index.js
@@ -1,3 +1,15 @@
-console.log('hola mundo');
+// ============= Nav Bar ============
 
-//
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+};
+
+// ========== Search Bar =========
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+};
