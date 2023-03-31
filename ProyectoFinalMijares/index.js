a96a1798c204ab169f05424e03ce6d06c34fcc5d
Julio Mijares <112848668+julioocmc@users.noreply.github.com>
2023-03-31 15:03:49 -0400

    Agregando Sweet Alert y Weather API

--- ProyectoFinalMijares/index.js
+++ ProyectoFinalMijares/index.js
@@ -13,3 +13,54 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
   searchForm.classList.toggle('active');
 };
+
+// ========== Form ==============
+
+const form = document.querySelector('#order-form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const requiredFields = form.querySelectorAll('[required]'); //validar
+  let formIsValid = true;
+
+  requiredFields.forEach((field) => {
+    if (!field.value) {
+      formIsValid = false;
+      field.classList.add('invalid');
+    } else {
+      field.classList.remove('invalid');
+    }
+  });
+
+  if (formIsValid) {
+    Swal.fire({
+      title: '¡Orden en proceso!',
+      text: 'Tu orden está siendo procesada',
+      icon: 'success',
+      confirmButtonText: 'OK',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        form.submit();
+      }
+    });
+  } else {
+    Swal.fire({
+      title: '¡Error!',
+      text: 'Por favor, complete todos los campos obligatorios',
+      icon: 'error',
+      confirmButtonText: 'OK',
+    });
+  }
+});
+
+// ===================== Weather ============
+
+fetch(
+  'https://api.weatherapi.com/v1/current.json?key=7165c9d76b084881b9764859233003&q=Caracas'
+)
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById('weather').textContent = `${data.current.temp_c}°C`;
+  })
+  .catch((error) => console.dir(error));
